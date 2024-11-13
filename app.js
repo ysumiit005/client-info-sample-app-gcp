1d4175a4c3eafa04d4aac3f06b528902b584949a
Derek Mizak <dm@emizak.com>
2024-11-13 11:48:24 +0000

    small edit of app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const requestIp = require('request-ip');
 const axios = require('axios');
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
